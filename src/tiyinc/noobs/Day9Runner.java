fedb3961af4e841fb8895ab53f8917c0cdbe7fd9
Dominique Bashizi <dominique@blupods.com>
2016-08-18 11:06:26 -0400

    Overdoing commits a bit here

--- src/tiyinc/noobs/Day9Runner.java
+++ src/tiyinc/noobs/Day9Runner.java
@@ -7,5 +7,13 @@ public class Day9Runner {
 
     public static void main(String[] args) {
         System.out.println("Running ...");
+        Day9Runner myRunner = new Day9Runner();
+        myRunner.testWriteFile();
+    }
+
+    public void testWriteFile() {
+        System.out.println("testWriteFile()");
+
+        
     }
 }
