22ded4ebf06f1437c4fc39c9871069c561ffd545
Dominique Bashizi <dominique@blupods.com>
2016-08-18 11:03:22 -0400

    Base version of Day9Runner

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,11 @@
+package tiyinc.noobs;
+
+/**
+ * Created by dbashizi on 8/18/16.
+ */
+public class Day9Runner {
+
+    public static void main(String[] args) {
+        System.out.println("Running ...");
+    }
+}
